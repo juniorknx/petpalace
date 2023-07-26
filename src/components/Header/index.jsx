004d2ed3d002afb63919d3e1cee9e012dc4ef3bf
Julio Junior <julio@Mac-mini-de-Julio.local>
2023-07-26 13:56:04 -0300

    Inserindo cabecalho v1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,36 @@
 import styles from './styles.module.css'
 import logo from '../../assets/logo.svg'
 import { Container } from '../Container'
+import { Link } from 'react-router-dom'
 
 export function Header() {
     return (
         <Container>
-            <img src={logo} alt='logo' />
+            <header className={styles.header__container}>
+                <img src={logo} alt='logo' />
+
+                <nav>
+                    <Link to={'/'} className={styles.nav__link}>
+                        Início
+                    </Link>
+
+                    <Link to={'/'} className={styles.nav__link}>
+                        Adote
+                    </Link>
+
+                    <Link to={'/'} className={styles.nav__link}>
+                        Ajude-me
+                    </Link>
+                </nav>
+
+                <div>
+                    <button>
+                        <Link to="/login">
+                            Login
+                        </Link>
+                    </button>
+                </div>
+            </header>
         </Container>
     )
 }
