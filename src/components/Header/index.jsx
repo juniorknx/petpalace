4d92cf475e1450d9d6d6d8224fdc3c3b090d537c
Julio Junior <julio@Mac-mini-de-Julio.local>
2023-07-31 22:47:05 -0300

    Criando componente de profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,14 @@ import styles from './styles.module.css'
 import logo from '../../assets/logo.svg'
 import { Container } from '../Container'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import { FaRegUserCircle } from 'react-icons/fa'
 
 export function Header() {
+
+    const { signed, user } = useContext(AuthContext)
+
     return (
         <Container>
             <header className={styles.header__container}>
@@ -24,11 +30,25 @@ export function Header() {
                 </nav>
 
                 <div>
-                    <button>
+                    {!signed && <button>
                         <Link to="/login">
                             Login
                         </Link>
-                    </button>
+                    </button>}
+
+                    {signed &&
+                        <div className={styles.profile__container}>
+                            <div className={styles.avatar}>
+                                <FaRegUserCircle size={25} color='#000' />
+                            </div>
+                            <div className={styles.username__container}>
+                                <Link to="/dashboard">
+                                    <span>Olá {user.name}</span>
+                                    <p>Minha Conta</p>
+                                </Link>
+                            </div>
+                        </div>
+                    }
                 </div>
             </header>
         </Container>
