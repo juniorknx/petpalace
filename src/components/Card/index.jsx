c1e8c565e44c1c35d450c25b5a341bb5f92db2c3
Julio Junior <julio@Mac-mini-de-Julio.local>
2023-08-30 02:37:42 -0300

    quebrando card em um componente e ajustes de css

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './styles.module.css';
+import { Link } from 'react-router-dom';
+import { MdOutlinePets } from 'react-icons/md';
+import { CiLocationOn } from 'react-icons/ci';
+
+const Card = React.memo(({ data }) => {
+    return (
+        <div className={styles.feed__grid}>
+            {data.map((item) => (
+                <Link key={item.id} to={`/pet/${item.id}`} style={{ textDecoration: 'none' }}>
+                    <div key={item.id} className={styles.feed_card}>
+                        <span className={item.disponivel ? styles.adopt_green : styles.adopt_red}>
+                            {item.disponivel ? 'Disponível para adoção' : 'Felizmente esse peludo foi adotado!'}
+                        </span>
+                        <img src={item.images[0].url} alt={item.raca} loading='lazy' />
+                        <div className={styles.card__title}>
+                            <MdOutlinePets size={17} color='#FFBD59' />
+                            <p>{item.nome.toLowerCase()}</p>
+                        </div>
+                        <div className={styles.card__title}>
+                            <CiLocationOn size={17} color='#FFBD59' />
+                            <p>{item.cidade} - {item.estado}</p>
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    );
+});
+
+export default Card;
